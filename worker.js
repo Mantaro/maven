62b67e9405ff07e9fa69ace9907d2dab0d79091d
natanbc <natanbc@users.noreply.github.com>
2021-04-03 22:40:38 -0300

    Don't cache maven-metadata.xml

--- worker.js
+++ worker.js
@@ -2,6 +2,13 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event))
 })
 
+function shouldCache(url) {
+    if(url.pathname.includes("/maven-metadata.xml")) {
+        return false;
+    }
+    return true;
+}
+
 async function handleRequest(event) {
   const request = event.request;
 
@@ -12,22 +19,29 @@ async function handleRequest(event) {
   const url = new URL(request.url);
   url.host = "maven.s3.nl-ams.scw.cloud";
 
+  if(!shouldCache(url)) {
+    console.log("URL", url.toString(), "not cacheable");
+    return await fetch(url, request);
+  }
+
   const cache = caches.default;
   const cacheKey = url.toString();
 
   let response = await cache.match(cacheKey);
 
   if(!response) {
-    console.log("not in cache, fetching from S3");
+    console.log("URL", url.toString(), "not in cache, fetching from S3");
     response = await fetch(url, request);
     response = new Response(response.body, response);
     response.headers.set("Cache-Control", "immutable, max-age=31536000");
     response.headers.set("X-Cache-Status", "miss");
     event.waitUntil(cache.put(cacheKey, response.clone()));
   } else {
+    console.log("URL", url.toString(), "in cache");
     response = new Response(response.body, response);
     response.headers.set("X-Cache-Status", "hit");
   }
-  
+
   return response;
 }
+
